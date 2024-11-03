e98a270d74087caff393f4176aadbbd05881663b
cherethod <sydig.reus@gmail.com>
2024-11-03 11:03:58 +0100

    added get prices logs

--- api/auction-prices.js
+++ api/auction-prices.js
@@ -10,7 +10,10 @@ export default async function handler(req, res) {
          const response = await axios.get(`https://pricing-api.tradeskillmaster.com/ah/${auctionHouseId}/item/${itemId}`, {
             headers: { 'Authorization': `Bearer ${accessToken}` }
          });
-
+        console.log('response: ',response);
+        console.log('data: ', response.data);
+        
+        
          res.status(200).json(response.data);
       } catch (error) {
          console.error('Error fetching prices:', error);
