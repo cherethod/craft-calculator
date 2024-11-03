c4ce85733717d7363ee553c0b2d83178970172b5
cherethod <sydig.reus@gmail.com>
2024-11-03 11:11:53 +0100

    removed env variables

--- api/authHelper.js
+++ api/authHelper.js
@@ -10,10 +10,10 @@ export async function getAccessToken() {
    if (!accessToken || currentTime >= tokenExpiresAt) {
       try {
          const response = await axios.post('https://auth.tradeskillmaster.com/oauth2/token', {
-            client_id: process.env.API_CLIENT_ID,
+            client_id: 'c260f00d-1071-409a-992f-dda2e5498536',
             grant_type: 'api_token',
             scope: 'app:realm-api app:pricing-api',
-            token: process.env.API_TOKEN
+            token: 'aa8e585e-7464-480e-afc7-c97657dff57e'
          });
          accessToken = response.data.access_token;
          tokenExpiresAt = currentTime + response.data.expires_in;
