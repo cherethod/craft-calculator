45c83df46f5e2d1ff438261a1626a41fe6c70bb4
cherethod <sydig.reus@gmail.com>
2024-11-04 23:40:02 +0100

    backup

--- src/hooks/useAPI.js
+++ src/hooks/useAPI.js
@@ -10,39 +10,60 @@ const useAPI = () => {
     const [regions, setRegions] = useState([]);
     const [realms, setRealms] = useState([]);
     const [auctionHouses, setAuctionHouses] = useState([]);
-    const { getAuthToken } = useAuth();
+    const { accessToken, getAuthToken } = useAuth();
 
-    const [selectedMode, setSelectedMode] = useState("search_settings");
+    const [selectedMode, setSelectedMode] = useState(!selectedRegion || !selectedRealm || !selectedAuctionHouse ? "search_settings" : "default");
 
     const handleSelectMode = (mode) => {
-        if (selectedRegion && selectedRealm && selectedAuctionHouse) {
-            setSelectedMode(mode);
-        }
-    };
+        if (!selectedRegion || !selectedRealm || !selectedAuctionHouse)  return;
+      setSelectedMode(mode);
+    }
 
     useEffect(() => {
-        const fetchToken = async () => {
-            try {
-                const newToken = await getAuthToken();
-                setToken(newToken);
-                console.log("Token obtenido:", newToken);
-            } catch (error) {
-                console.error("Error al obtener el token de autenticación:", error);
-            }
-        };
+            // const storedToken = localStorage.getItem('token');
+            // if (storedToken) {
+            //     setToken(storedToken);
+            // }
 
+            // else {
+            const fetchToken = async () => {
+            try {
+                const authToken = await getAuthToken();
+                console.log('authToken:', authToken);
+                if (authToken) {
+                    setToken(authToken);
+                    // localStorage.setItem('token', authToken);
+                }
+            } catch (error) {
+                console.error('Error getting token:', error);
+
+                
+            }
+                // const authToken = getAuthToken()
+                //     .then((newToken) => {
+                //         setToken(newToken);
+                //         // localStorage.setItem('token', newToken);
+                //     })
+                //     .catch((error) => {
+                //         console.error('Error getting token:', error);
+                //     });
+                // setToken(authToken);
+            } 
         if (!token) {
             fetchToken();
         }
-    }, [getAuthToken, token]);
 
+        // }        
+    }, []);
+
+    
     useEffect(() => {
         const fetchRealms = async () => {
             if (token) {
                 try {
                     const regionsData = await getRealms();
                     console.log('Regions:', regionsData.items);  // Ahora debería mostrar los datos correctos
-                    setRegions(regionsData.items || regionsData);  // Asegúrate de que la estructura `items` sea correcta
+                    setRegions(regionsData.items);  // Asegúrate de que la estructura `items` sea correcta
                 } catch (error) {
                     console.error('Error fetching regions:', error);
                 }
@@ -79,100 +100,105 @@ const useAPI = () => {
         
     }, [regions, realms, auctionHouses]);
 
+
 // // Temp dev data load
 //     useEffect(() => {
 //         setRegions(regionsData);
 //     }, []);
 
-    const handleTokenChange = (newToken) => {
-        setToken(newToken);
-    };
+        const handleTokenChange = (newToken) => {
+            setToken(newToken);
+        };
 
-    const handleRegionChange = (regionSelection) => {
-        const selectedRegion = Object.values(regions).find(region => region.regionId === regionSelection);
-        if (selectedRegion) {
-            console.log('inside if');                
-            setSelectedRegion(selectedRegion.regionId);
-            setRealms(selectedRegion.realms);
-        } else {
-            console.log('inside else');                
-            setSelectedRegion(null);
-            setRealms([]);
+        const handleRegionChange = (regionSelection) => {
+            const selectedRegion = Object.values(regions).find(region => region.regionId === regionSelection);
+            if (selectedRegion) {
+                console.log('inside if');                
+                setSelectedRegion(selectedRegion.regionId);
+                setRealms(selectedRegion.realms);
+            } else {
+                console.log('inside else');                
+                setSelectedRegion(null);
+                setRealms([]);
+            }
+        };
+
+        const handleRealmChange = (realmName) => {
+            const realmEntry = Object.values(realms).find(realm => realm.name === realmName);
+            if (realmEntry) {
+                setSelectedRealm(realmEntry.realmId); // Guarda solo el realmId
+                setAuctionHouses(realmEntry.auctionHouses);
+                
+            } else {
+                setSelectedRealm(null); // Si no encuentra coincidencia, limpia la selección
+                setAuctionHouses([]); // Limpia las casas de subastas
+            }
+        };
+        
+
+        const handleAuctionHouseIdChange = (newAuctionHouseId) => {
+            console.log('newAuctionHouseId: ',newAuctionHouseId);
+            
+            setSelectedAuctionHouse(newAuctionHouseId);
+        };
+
+        const handleSubmitSearchSettings = (e) => {
+            e.preventDefault();
+            if (selectedRegion && selectedRealm && selectedAuctionHouse) {
+                console.log('selectedRegion: ',selectedRegion);
+                console.log('selectedRealm: ',selectedRealm);
+                console.log('selectedAuctionHouse: ',selectedAuctionHouse);
+            }
         }
-    };
 
-    const handleRealmChange = (realmName) => {
-        const realmEntry = Object.values(realms).find(realm => realm.name === realmName);
-        if (realmEntry) {
-            setSelectedRealm(realmEntry.realmId); // Guarda solo el realmId
-            setAuctionHouses(realmEntry.auctionHouses);
-        } else {
-            setSelectedRealm(null); // Si no encuentra coincidencia, limpia la selección
-            setAuctionHouses([]); // Limpia las casas de subastas
+        const handleStoreSettings = () => {
+            console.log('Settings stored:', selectedRegion, selectedRealm, selectedAuctionHouse);
+            localStorage.setItem('selectedRegion', selectedRegion);
+            localStorage.setItem('selectedRealm', selectedRealm);
+            localStorage.setItem('selectedAuctionHouse', selectedAuctionHouse);
         }
-    };
 
-    const handleAuctionHouseIdChange = (newAuctionHouseId) => {
-        console.log('newAuctionHouseId: ',newAuctionHouseId);
-        setSelectedAuctionHouse(newAuctionHouseId);
-    };
 
-    const handleSubmitSearchSettings = (e) => {
-        e.preventDefault();
-        if (selectedRegion && selectedRealm && selectedAuctionHouse) {
-            console.log('selectedRegion: ',selectedRegion);
-            console.log('selectedRealm: ',selectedRealm);
-            console.log('selectedAuctionHouse: ',selectedAuctionHouse);
-        }
-    }
 
-    const handleStoreSettings = () => {
-        console.log('Settings stored:', selectedRegion, selectedRealm, selectedAuctionHouse);
-        localStorage.setItem('selectedRegion', selectedRegion);
-        localStorage.setItem('selectedRealm', selectedRealm);
-        localStorage.setItem('selectedAuctionHouse', selectedAuctionHouse);
-    }
+        const getAuctionPrices = async (auctionHouseId, itemId) => {
+            try {
+              const res = await fetch(`https://craft-calculator-puce.vercel.app/api/auction-prices?auctionHouseId=${auctionHouseId}&itemId=${itemId}`);
+              const data = await res.json();
+              return data;
+            } catch (error) {
+              console.error('Error fetching auction prices:', error);  // Error while fetching auction prices
+            }
+          };
 
-    const getAuctionPrices = async (auctionHouseId, itemId) => {
-        try {
-            const res = await fetch(`https://craft-calculator-puce.vercel.app/api/auction-prices?auctionHouseId=${auctionHouseId}&itemId=${itemId}`);
-            const data = await res.json();
-            return data;
-        } catch (error) {
-            console.error('Error fetching auction prices:', error);  // Error while fetching auction prices
-        }
-    };
-
-    const getRealms = async () => {
-        try {
+          const getRealms = async () => {
             const res = await fetch('https://craft-calculator-puce.vercel.app/api/realms');
             if (!res.ok) throw new Error('Failed to fetch realms');
             const data = await res.json();
             return data;
-        } catch (error) {
-            console.error("Error fetching realms:", error);
-        }
-    };
+        };
 
-    return {
-        token,
-        selectedRegion,
-        selectedRealm,
-        selectedAuctionHouse,
-        regions,
-        realms,
-        auctionHouses,
-        selectedMode,
-        handleSelectMode,
-        handleTokenChange,
-        handleRegionChange,
-        handleRealmChange,
-        handleAuctionHouseIdChange,
-        handleSubmitSearchSettings,
-        handleStoreSettings,
-        getAuctionPrices,
-        getRealms,
-    };
-}
+        
 
+        return {
+            token,
+            selectedRegion,
+            selectedRealm,
+            selectedAuctionHouse,
+            regions,
+            realms,
+            auctionHouses,
+            selectedMode,
+            handleSelectMode,
+            handleTokenChange,
+            handleRegionChange,
+            handleRealmChange,
+            handleAuctionHouseIdChange,
+            handleSubmitSearchSettings,
+            handleStoreSettings,
+            getAuctionPrices,
+            getRealms,
+        };
+        
+    }
+ 
 export default useAPI;
