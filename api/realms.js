c61a52b407d45327e2f703211b3a842c1b3261b8
cherethod <sydig.reus@gmail.com>
2024-11-07 00:38:00 +0100

    accestoken is in realms endpoint?

--- api/realms.js
+++ api/realms.js
@@ -4,7 +4,8 @@ import axios from 'axios';
 export default async function handler(req, res) {
     if (req.method === 'GET') {
         const { accessToken } = req.headers;
-
+        console.log('Token de acceso:', accessToken);
+        
         if (!accessToken) {
             return res.status(400).json({ message: 'Access token is required' });
         }
